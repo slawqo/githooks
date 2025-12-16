b736c057d005ac506e651509dea9e601e53a3431
Slawek Kaplonski <skaplons@redhat.com>
2025-12-16 14:40:46 +0100

    Add hook to prepare draft of the commit message

---
+++ hooks/prepare-commit-msg-hooks/prepare_commit_msg.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import os
+import textwrap
+
+import ollama
+
+LINE_LENGTH = 79
+DEBUG_FLAG = "DEBUG_GITHOOKS"
+
+LLM_MODEL="llama3.1"
+LLM_URL="http://localhost:11434"
+
+SYSTEM_PROMPT="""
+You are helpful assistant of the software developer. Your job is to analyze
+changes which are going to be commited to the project's repository and based
+on that propose some initial commit message draft.
+
+<rules>
+- user message contains always 2 things:
+    - branch name inside <branch></branch> tags,
+    - diff which is going to be commited, inside <diff></diff> tags,
+
+- your proposal of the commit message have to always be in english language
+  only,
+- you have to propose commit message in three parts:
+    - commit_msg_title - this is what will be placed as first line of the
+      git commit message and is always mandatory,
+    - commit_msg - this is part with detailed description of the change,
+      it is optional, you can omit it sometimes if changes are small and
+      self-explanatory,
+    - related issue - this should contain number of the related bug or new
+      feature from some bug tracking system, it is optional, put empty string
+      if you will not find anything like that,
+- your proposal of the commit message have to be as short as possible, not
+  longer then it is really needed,
+
+- your response have to be in JSON format, with schema like below:
+    {
+        "_reasoning": "put your thinking here, for debugging purpose",
+        "commit_msg_title": "Title of the commit message you propose",
+        "commit_msg": "The rest of the commit message you propose"
+    }
+</rules>
+
+<examples>
+    <example>
+        <input>
+            <diff>
+                diff --git a/file1.txt b/file1.txt
+                index 1234567..89abcdef 100644
+                --- a/file1.txt
+                +++ b/file1.txt
+                @@ -1,1 +1,1 @@
+                -def test_method()
+                +def better_test_method()
+            </diff>
+            <branch>
+            issue/123454
+            </branch>
+        </input>
+        <output>
+        {
+            "_reasoning": "your reasoning, with detailed description how you "
+                          "have came to the final conclusion",
+            "commit_msg_title": "Replace test_method with new implementation",
+            "commit_msg": "This patch replaces old test_method with new one, named better_test_method.",
+            "related_issue": "1234567"
+        }
+        </output>
+
+    </example>
+</examples>
+
+"""
+
+
+def is_debug_enabled():
+    debug_flag = os.environ.get(DEBUG_FLAG)
+    return debug_flag.lower() in ["1", "yes", "true"]
+
+
+def get_args():
+    if len(sys.argv) < 4:
+        # no diff or commit message file provided, nothing to do
+        exit(0)
+    commit_msg_file = sys.argv[1]
+    if not commit_msg_file:
+        # no commit message file provided, nothing to do
+        exit(0)
+    git_branch = sys.argv[2]
+    diff = sys.argv[3]
+    if not diff:
+        # no diff provided, nothing to do
+        exit(0)
+    return commit_msg_file, git_branch, diff
+
+
+def prepare_commit_msg_draft(title, content, related_issue):
+    commit_msg = f"""
+#### This is just draft of the commit msg made by AI ####
+
+{title}"""
+    if content:
+        commit_msg = f"{commit_msg}\n"
+        wrapped_content = textwrap.wrap(content, LINE_LENGTH)
+        for line in wrapped_content:
+            commit_msg = f"{commit_msg}\n{line}"
+    if related_issue:
+        commit_msg = f"{commit_msg}\n\nCloses: #{related_issue}"
+    return commit_msg
+
+
+def prepare_commit_msg_file(file_path, commit_msg):
+    with open(file_path, "a") as f:
+        f.seek(0)
+        f.write(commit_msg)
+
+
+def main():
+
+    commit_msg_file, git_branch, diff = get_args()
+    input_msg = f"<branch>{git_branch}</branch><diff>{diff}</diff>"
+
+    client = ollama.Client(host=LLM_URL)
+    llm_response: ollama.ChatResponse = client.chat(
+        model=LLM_MODEL,
+        stream=False,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM_PROMPT
+            },
+            {
+                "role": "user",
+                "content": input_msg
+            }
+        ]
+    )
+
+    try:
+        response_json = json.loads(llm_response.message.content)
+    except json.JSONDecodeError:
+        if is_debug_enabled():
+            print(f"Error: Invalid JSON response: {llm_response.message.content}")
+        # Don't block commit if JSON is not valid
+        exit(0)
+
+    if is_debug_enabled():
+        print("--------------------------------")
+        print(f"Reasoning: {response_json['_reasoning']}")
+        print("--------------------------------")
+
+    commit_msg = prepare_commit_msg_draft(
+        response_json['commit_msg_title'],
+        response_json.get('commit_msg', ''),
+        response_json.get('related_issue', ''))
+
+    prepare_commit_msg_file(commit_msg_file, commit_msg)
+
+
+if __name__ == "__main__":
+    main()
--- install.sh
+++ install.sh
@@ -16,13 +16,22 @@ fi
 
 HOOKS_PATH="$DEST_REPO_PATH/.git/hooks"
 PRE_COMMIT_HOOKS_PATH="$HOOKS_PATH/pre-commit.d"
+PREPARE_COMMIT_MSG_HOOKS_PATH="$HOOKS_PATH/prepare-commit-msg.d"
 
+# TODO: add config option to specify which hooks to install
+# Install pre-commit hooks
 if [ ! -d "$PRE_COMMIT_HOOKS_PATH" ]; then
     mkdir -p "$PRE_COMMIT_HOOKS_PATH"
 fi
-
 cp -r hooks/pre-commit "$HOOKS_PATH/pre-commit"
 cp -r hooks/pre-commit-hooks/* "$PRE_COMMIT_HOOKS_PATH/"
+
+# Install prepare-commit-msg hooks
+if [ ! -d "$PREPARE_COMMIT_MSG_HOOKS_PATH" ]; then
+    mkdir -p "$PREPARE_COMMIT_MSG_HOOKS_PATH"
+fi
+cp -r hooks/prepare-commit-msg "$HOOKS_PATH/prepare-commit-msg"
+cp -r hooks/prepare-commit-msg-hooks/* "$PREPARE_COMMIT_MSG_HOOKS_PATH/"
 
 echo "Pre-commit hooks installed successfully"
 exit 0
